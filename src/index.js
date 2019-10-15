f6d688df5618f789fd047e811ea5cbef5b46b737
Askhabali.Magomachiev <fumufu86@gmail.com>
2019-10-15 20:05:24 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
 const actual = readlineSync.question('May I have your name? ');
 
-console.log(`Hello, ${actual}!`);
-
-export {
-actual,
-};
+export default actual;
