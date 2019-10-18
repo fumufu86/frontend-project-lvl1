5bf0252008f3561fa4538201bc5b9df7f56497d4
Askhabali.Magomachiev <fumufu86@gmail.com>
2019-10-18 23:17:23 +0300

    refactoring

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import gameeven from '../even';
+import gameeven from '../games/even';
 
 gameeven();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
 
-const greeting = () => {
+const countround = 3;
+const getquestion = (qa) => car(qa);
+const getcorrectanswer = (qa) => cdr(qa);
+
+const playgame = (rules, qanda) => {
   console.log('Welcome to the Brain Games!');
   const actual = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${actual}!`);
+  console.log(rules);
+  for (let count = countround; count > 0; count -= 1) {
+    const getQandA = qanda();
+    const question = getquestion(getQandA);
+    const correctanswer = getcorrectanswer(getQandA);
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (correctanswer !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctanswer}'.`);
+      console.log(`Let's try again, ${actual}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${actual}!`);
 };
-
-export default greeting;
+export default playgame;
