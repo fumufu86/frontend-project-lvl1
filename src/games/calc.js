da3f97e839b0b47e416b4afacc8d3ec6f669b57c
Askhabali.Magomachiev <fumufu86@gmail.com>
2019-10-21 22:45:31 +0300

    fix braincalc

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,22 +4,22 @@ import generateNumber from '../utils';
 
 
 const minrandom = 1;
-const maxrandom = 999;
+const maxrandom = 9;
 const operand = '+-*';
 
 
 const rules = 'What is the result of the expression?';
+
 
 const qanda = () => {
   const firstnumber = generateNumber(minrandom, maxrandom);
   const lasttnumber = generateNumber(minrandom, maxrandom);
   const generateoperand = generateNumber(0, 2);
   const question = `${firstnumber} ${operand[generateoperand]} ${lasttnumber}`;
-  const answer = () => {
-    if (generateNumber === 0) return firstnumber + lasttnumber;
-    if (generateNumber === 1) return firstnumber - lasttnumber;
-    return firstnumber * lasttnumber;
-  };
+  let answer;
+  if (generateoperand === 0) answer = String(firstnumber + lasttnumber);
+  if (generateoperand === 1) answer = String(firstnumber - lasttnumber);
+  if (generateoperand === 2) answer = String(firstnumber * lasttnumber);
   return cons(question, answer);
 };
 
