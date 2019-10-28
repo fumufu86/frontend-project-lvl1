270aecb66b80bc3d898baf5690fdc49ba7f1528d
Askhabali.Magomachiev <fumufu86@gmail.com>
2019-10-28 19:20:51 +0300

    fix isEven function

--- src/games/even.js
+++ src/games/even.js
@@ -5,14 +5,14 @@ import generateNumber from '../utils';
 
 const minrandom = 1;
 const maxrandom = 999;
-const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+const isEven = (num) => num % 2 === 0;
 
 const RulesOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // quanda is question and answer.
 const QuestionAndAnswer = () => {
   const Question = generateNumber(minrandom, maxrandom);
-  const CorrectAnswer = isEven(Question);
+  const CorrectAnswer = isEven(Question) ? 'yes' : 'no';
   return cons(Question, CorrectAnswer);
 };
 
